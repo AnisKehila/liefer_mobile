e532583014de98e9e5292d0124ab119161727013
aniskehila <aniskehila22@gmail.com>
2024-02-27 23:18:39 +0100

    change toast library

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,13 @@
 import { Slot } from "expo-router";
 import React from "react";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Toast from "react-native-toast-message";
 
 const HomePageLayout = () => {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <>
       <Slot />
-    </GestureHandlerRootView>
+      <Toast />
+    </>
   );
 };
 
